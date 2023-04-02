b0ba369dc2986c481d24fd29ca8e785102962c0e
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-02 00:46:28 -0500

    feat(Contact): initial Contact Site

---
+++ my-site/src/Contact/Contact.js
@@ -0,0 +1,14 @@
+import './contact.css';
+
+
+function Contact() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default Contact;
