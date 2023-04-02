6ee241138bf84b33e95089006f11d03089c1a248
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-02 00:46:49 -0500

    feat(Home): initial Home site page

---
+++ my-site/src/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+
+function Home() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default Home;
