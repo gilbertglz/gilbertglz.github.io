6b66cf62b4719bd08dece1d74c85a9e6b067d786
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-02 00:45:39 -0500

    feat(App.js): made the site scrollable one page site

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import MyHeader from './Header/MyHeader';
+import { Element } from 'react-scroll';
+import Home from './Home/Home.js';
+
 
 function App() {
   return (
     <div className="App">
+      <MyHeader titles={['contact','links','projects','about me', 'home']}/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Element name="home">
+        <Home />
+      </Element>
+      <Element name="about me">
+        <Home />
+      </Element>
+      <Element name="projects">
+        <Home />
+      </Element>
+      <Element name="links">
+        <Home />
+      </Element>
+      <Element name="contact">
+        <Home />
+      </Element>
       </header>
     </div>
   );
