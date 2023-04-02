f4dfeb8e4ec187ded2110428dc7d482e53852356
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-02 00:47:14 -0500

    feat(Links): initial Links page site

---
+++ my-site/src/Links/Links.js
@@ -0,0 +1,14 @@
+import './Links.css';
+
+
+function Links() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default Links;
