bf3498ad1bb031f20cd25d8de04a34be066a4d9f
gilbertglz <74834795+gilbertglz@users.noreply.github.com>
2023-04-06 21:24:04 -0500

    feat(AboutMe): fixed the about me and added more detail

--- my-site/src/AboutMe/AboutMe.js
+++ my-site/src/AboutMe/AboutMe.js
@@ -12,44 +12,48 @@ function AboutMe() {
     { icon: <TbArrowLoopRight />, text: 'Perl' },
     { icon: <TbArrowLoopRight />, text: 'Python' },
   ];
+  const firstParagraph = <p className="" style={{fontSize:'20px', wordWrap:'break-word'}}>
+      Hi, I'm Gilbert, a software developer with a passion for automation,
+      machine learning, and UI design. I'm committed to delivering
+      high-quality software that meets user needs and exceeds
+      expectations.With a background in customer service, 
+      I bring a unique perspective to software development 
+      that prioritizes user feedback and continuous improvement. </p>;
+  const secondParagraph =
+  <p style={{fontSize:'20px', wordWrap:'break-word'}}>
+    My experience working at my university library helped me develop excellent communication and problem-solving skills, 
+    which have proven valuable in my work as a software developer. As an after-school program teacher, 
+    I honed my ability to explain complex ideas in a way that is accessible to all learners, 
+    which has helped me in designing user-friendly interfaces for software applications.
+  </p>;
 
   return (
     
     <div className="AboutMe">
-      <div className='wrapp'>
+      <div className='wrapp'></div>
+      <header className="aboutMeHeader">About Me</header>
+      <div class="gridWrap" style={{display:'grid'}}>
+        <div className="aboutMeHeaderContainer" >
+          {firstParagraph} 
+          {secondParagraph}
+        </div>  
+        
       </div>
-      <div className="aboutMeHeaderContainer">
-        <div className="aboutMeWords">
-          <header className="aboutMeHeader">About Me</header>
-          <p className="parOne">
-            Hi, I'm Gilbert, a software developer with a passion for automation,
-            machine learning, and UI design. I'm committed to delivering
-            high-quality software that meets user needs and exceeds
-            expectations.
-          </p>
-          <p className="parOne">
-            With a background in customer service, I bring a unique perspective
-            to software development that prioritizes user feedback and
-            continuous improvement. Check out my portfolio to see some of my
-            recent projects and accomplishments.
-          </p>
-        </div>
-        <div className="aboutMeImage">
-          <div className="imageContainer">
-            <img className="myImage" src={Photo} alt="Profile" />
+      <div style={{display:'grid',gridTemplateColumns:'1fr 1fr', overflow:'visible'}}>
+        <div style={{position:'relative'}}>
+        <p className="parTwo">Here is some of the tech I've been using recently:</p>
+          <div className="listContainer">
+            {contactList.map(({ icon, text }, index) => (
+              <div className="listLang" key={index}>
+                <div className="listIcon">{icon}</div>
+                <div className="listText">{text}</div>
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-
-      <p className="parTwo">Here is some of the tech I've been using recently:</p>
-
-      <div className="listContainer">
-        {contactList.map(({ icon, text }, index) => (
-          <div className="listLang" key={index}>
-            <div className="listIcon">{icon}</div>
-            <div className="listText">{text}</div>
+        <div className="imageContainer" style={{maxWidth: '100%', maxHeight:'100%'}}>
+                  <img className="myImage" src={Photo} alt="Profile" />
           </div>
-        ))}
       </div>
     </div>
   );
